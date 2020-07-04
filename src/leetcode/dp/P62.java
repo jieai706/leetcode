7f947b7a0cc59f6afa865f39b5f23ab0321b281f
liulang <897436665@qq.com>
2020-07-04 16:24:00 +0800

    新建DP，62移至dp

---
+++ src/leetcode/dp/P62.java
@@ -0,0 +1,17 @@
+package leetcode.dp;
+
+import java.util.UUID;
+
+/**
+ * Unique Paths
+ * 方法说明
+ * @author jieai706
+ * @date 2020-07-04
+ */
+public class P62 {
+
+	public static void main(String[] args) {
+		System.out.println(UUID.randomUUID().toString().replace("-", ""));
+		System.out.println(UUID.randomUUID().toString().replace("-", ""));
+	}
+}
